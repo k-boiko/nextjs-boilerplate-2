e3662037ea6d98e7a6c657ba9c5dc84c8e5b9e79
k-boiko <kboyko1999@gmail.com>
2025-05-12 13:45:14 +0100

    more visual response

--- app/page.tsx
+++ app/page.tsx
@@ -33,6 +33,8 @@ function fetchWithoutCache(url: string) {
 const apiUrl = 'https://nonexisting.vesnasoft.org/test';
 
 export default function Home() {
+  const [isLoading1, setIsLoading1] = useState(false);
+  const [isLoading2, setIsLoading2] = useState(false);
   const [state1, setState1] = useState(null);
   const [state2, setState2] = useState(null);
   const [error, setError] = useState(null);
@@ -42,8 +44,26 @@ export default function Home() {
       <p>state2: {JSON.stringify(state2)}</p>
       <p>error {JSON.stringify(error)}</p>
       <p>
-        <button onClick={() => fetchWithCache(apiUrl).then((data) => setState1(data)).catch((e) => setError(e))}>fetch with cache</button>
-        <button onClick={() => fetchWithoutCache(apiUrl).then((data) => setState2(data)).catch(e => setError(e))}>fetch without cache</button>
+        <button onClick={() => {
+          setIsLoading1(true);
+          fetchWithCache(apiUrl).then((data) => {
+            setState1(data)
+            setIsLoading1
+        }).catch((e) => {
+          setIsLoading1(false)
+          setError(e)
+        })
+        }}>fetch with cache {isLoading1 && '...Loading...'}</button> <br />
+        <button onClick={() => {
+          setIsLoading2(true);
+          fetchWithoutCache(apiUrl).then((data) => {
+            setIsLoading2(false);
+            setState2(data)
+        }).catch(e => {
+          setIsLoading2(false)
+          setError(e)
+        })
+        }}>fetch without cache {isLoading2 && '...loading...'}</button>
       </p>
     </div>
   );
